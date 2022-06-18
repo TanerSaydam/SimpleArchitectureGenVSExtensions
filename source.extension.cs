28b0922a3bec59e501dccbcaa6d9cb987e7056e1
Taner Saydam <tanersaydam@gmail.com>
2022-06-19 01:40:57 +0300

    .

--- source.extension.cs
+++ source.extension.cs
@@ -8,8 +8,8 @@ namespace SimpleArchitectureGen
     internal sealed partial class Vsix
     {
         public const string Id = "SimpleArchitectureGenv1.af0b02be-d43b-45d1-ba43-7d214dd9c2bf";
-        public const string Name = "Simple Architecture Code Generator V.1.3";
-        public const string Description = @"This is code generator for SimpleArchitecture V.1.3";
+        public const string Name = "Simple Architecture Code Generator V.1.4";
+        public const string Description = @"This is code generator for SimpleArchitecture V.1.4";
         public const string Language = "en-US";
         public const string Version = "1.0";
         public const string Author = "Taner Saydam";
